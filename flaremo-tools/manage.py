99f1916b4bab9357a8c867e75f256db5818340a3
printinggraphicsdesigner <printinggraphicsdesigner@gmail.com>
2026-04-12 19:36:52 +0600

    Update manage.py

--- flaremo-tools/manage.py
+++ flaremo-tools/manage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-if _name_ == '_main_':
+
+def main():
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tools_hub.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -13,3 +16,7 @@ if _name_ == '_main_':
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
