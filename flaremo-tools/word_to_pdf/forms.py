ed54178f5e5b4fa0598c295d563c3d936716778b
printinggraphicsdesigner <printinggraphicsdesigner@gmail.com>
2026-04-15 19:26:04 +0600

    Update forms.py

--- flaremo-tools/word_to_pdf/forms.py
+++ flaremo-tools/word_to_pdf/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class WordToPdfForm(forms.Form):
     word_file = forms.FileField(
-        label="Upload Word File (.docx)",
-        help_text="only .docx format supported"
+        label="Upload Word File(.docx)",
+        help_text="only.docx format supported"
     )
