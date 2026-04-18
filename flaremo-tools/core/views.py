6641ab80511552b414b86e6cb7e27646a32e62d1
printinggraphicsdesigner <printinggraphicsdesigner@gmail.com>
2026-04-18 16:09:00 +0600

    Delete flaremo-tools directory

--- flaremo-tools/core/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Tool   # 👈 ADD
-
-def home(request):
-    tools = Tool.objects.filter(is_active=True)   # 👈 CHANGE
-    return render(request, 'home.html', {'tools': tools})
