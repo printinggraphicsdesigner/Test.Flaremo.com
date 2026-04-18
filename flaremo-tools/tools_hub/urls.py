61409984fdec7ded98e6371a4cff8801777895a4
printinggraphicsdesigner <printinggraphicsdesigner@gmail.com>
2026-04-18 16:29:35 +0600

    Delete flaremo-tools directory

--- flaremo-tools/tools_hub/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('word-to-pdf/', include('word_to_pdf.urls')),
-]
