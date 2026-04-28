9f5ee1e556edccef95641ae9e8a303670b0b24fb
printinggraphicsdesigner <printinggraphicsdesigner@gmail.com>
2026-04-28 18:04:27 +0600

    Delete flaremo-tools directory

--- flaremo-tools/pages/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Page
-
-@admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'is_published']
-    prepopulated_fields = {"slug": ("title",)}
