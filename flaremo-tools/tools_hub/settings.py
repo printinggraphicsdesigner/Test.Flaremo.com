2090fd35d0cabe693f4ec630c5935ffc0f7e8046
printinggraphicsdesigner <printinggraphicsdesigner@gmail.com>
2026-04-29 19:00:37 +0600

    Update settings.py

--- flaremo-tools/tools_hub/settings.py
+++ flaremo-tools/tools_hub/settings.py
@@ -76,7 +76,8 @@ DATABASES = {
 from decouple import config
 
 # ইমেইল সার্ভার সেটিংস
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'mail.flaremo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
