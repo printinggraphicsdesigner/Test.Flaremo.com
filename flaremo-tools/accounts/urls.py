4a2ed09e29c3d5de88826aa73e2503f1e2bdf21c
printinggraphicsdesigner <printinggraphicsdesigner@gmail.com>
2026-04-29 11:32:45 +0600

    Delete flaremo-tools directory

--- flaremo-tools/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='account_dashboard'),
-    path('verify-otp/<int:user_id>/', views.verify_otp, name='verify_otp'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('logout/', views.logout_view, name='logout'),
-]
--- flaremo-tools/image_to_video/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.image_to_video_view, name='image_to_video'),
-]
